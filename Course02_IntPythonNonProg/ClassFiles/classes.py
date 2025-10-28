367387001f968d5857c3f14bbdf9537d9d472c10
kj615 <88256157+kj615@users.noreply.github.com>
2025-10-28 23:05:29 +0000

    new file:   Course02_IntPythonNonProg/ClassFiles/classes.py

---
+++ Course02_IntPythonNonProg/ClassFiles/classes.py
@@ -0,0 +1,14 @@
+class Dog:
+    info = "A furry friend"
+
+
+print(Dog.info)
+
+# Challenge:
+# Create a class for something around you in the room.
+# Create a class variable inside the class
+
+class Office:
+    furniture = "couch"
+
+print(Office.furniture)
